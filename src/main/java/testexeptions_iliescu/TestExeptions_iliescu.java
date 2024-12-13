22821bb1dea8dc89682880015331530c4ed34d2b
G1OsU <giosue.laurenzi@risikamente.it>
2024-12-13 09:38:40 +0100

    first commit

---
+++ src/main/java/testexeptions_iliescu/TestExeptions_iliescu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package testexeptions_iliescu;
+
+/**
+ *
+ * @author B045_15
+ */
+public class TestExeptions_iliescu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
